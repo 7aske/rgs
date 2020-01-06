97ef11bfa064fd74ea2ddd85e3036fc80fd7986c
Nikola Tasic <ntasic7@gmail.com>
2020-01-06 07:38:05 +0100

    initial, barebones working (no color)

---
+++ README.md
@@ -0,0 +1,98 @@
+# rgs
+
+## Description
+
+`rgs` (recheck-git-status) is a simple tool for us forgetful people. Namely what the tool does is checks a folder with git repositories for ones that have been changed but not committed. All that to avoid unnecessary merges and conflicts.
+<small>Originally `cgs`, written in C.</small>
+## Dependencies
+
+Git - obviously
+
+Unix based system
+
+## Usage
+
+```
+rgs <flags>
+```
+By default `rgs` assumes that repositories are categorized in one of the following way in a root folder as show by the tree below.
+
+```
+root_folder
+├── category1
+│   ├── repository1
+│   ├── repository2
+│   ├── repository3
+│   ├── repository4
+│   └── repository5
+├── category2
+│   ├── repository1
+│   ├── repository2
+│   ├── repository3
+│   ├── repository4
+│   ├── repository5
+│   └── repository6
+├── repository1
+└── repository2
+```
+
+Repository must be either the first sub-folder in the root folder or the first sub-folder of a categorizing folder (whether it was sorted by language or anything else). Root folder is being determined by the `CODE` environmental variable and it is recommended to have it set up in your `.bashrc` file as follows:
+
+```
+export CODE=/path/to/your/repos/folder
+```
+
+Otherwise it will not work as it was designed to require minimal typing in the actual command line to work.
+
+Output of the program is nothing if there are no commits to be made or a listing of all repositories and their respective categorizing folders:
+
+```
+category1             repo5
+category4             repo1
+repo1
+repo3
+```
+
+### Additional flags
+
+Few options are available that pretty print the stats:
+
+`-l` - shows all categories and number of repositories in them. Below that are listed all the uncommitted repositories.
+
+```
+category1           2
+category1          repo1
+category2           5
+category3          22
+category3          repo2  
+category3          repo5  
+category3          repo8
+category4          12
+category5           3
+
+  
+  
+```
+
+`-ll` - shows all categories and their repositories in a tree like form (same as `tree` linux program) with uncommitted ones being highlighted in red:
+
+```
+.
+├── category1 (5)
+│   ├── repository1
+│   ├── repository2
+│   ├── repository3
+│   ├── repository4
+│   └── repository5
+├── category2 (6)
+│   ├── repository1
+│   ├── repository2
+│   ├── repository3
+│   ├── repository4
+│   ├── repository5
+│   └── repository6
+├── repository1 (1)
+└── repository2 (1)
+```
+
+`-d` - outputs the absolute path to the repository. Assumes `--no-color`. Useful for `dmenu` scripting.
---
+++ src/main.rs
@@ -0,0 +1,206 @@
+use std::io;
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process;
+
+struct Proj {
+    name: String,
+    path: String,
+    is_ok: bool,
+}
+
+struct Lang {
+    name: String,
+    projs: Vec<Proj>,
+}
+
+impl Proj {
+    fn new(name: &str, path: &str) -> Self {
+        Proj {
+            name: String::from(name),
+            path: String::from(path),
+            is_ok: true,
+        }
+    }
+}
+
+impl Lang {
+    fn new(name: &str) -> Self {
+        Lang {
+            name: String::from(name),
+            projs: vec![],
+        }
+    }
+    fn add_proj(&mut self, proj: Proj) {
+        self.projs.push(proj)
+    }
+}
+
+
+static IGNORED: [&str; 1] = ["_test"];
+
+
+fn main() {
+    let mut color: bool = true;
+    let mut long_print: bool = false;
+    let mut long_long_print: bool = false;
+    let mut dir_print: bool = false;
+
+    let mut langs = Vec::new();
+    let mut count = 0;
+    let code = match env::var("CODE") {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("ERROR: Cannot find 'CODE' environmental variable");
+            process::exit(1);
+        }
+    };
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {}
+        _ => {
+            for arg in args {
+                match arg.as_str() {
+                    "-l" => long_print = true,
+                    "-ll" => long_long_print = true,
+                    "--no-color" => color = false,
+                    "-d" => dir_print = true,
+                    _ => {}
+                }
+            }
+        }
+    }
+
+
+    list_dir(code.as_str(), 2, &mut count, &mut langs).expect("ERROR: Failed reading 'CODE' directory.");
+
+    for l in &mut langs {
+        for p in &mut l.projs {
+            check_status(p);
+        }
+    }
+
+    if !long_long_print {
+        for l in &mut langs {
+            if long_print {
+                println!("{:16} {:16}", l.name, l.projs.len());
+            }
+            for p in &mut l.projs {
+                if !p.is_ok {
+                    if dir_print {
+                        println!("{:32}", p.path);
+                    } else {
+                        println!("{:16} {:16}", l.name, p.name);
+                    }
+                }
+            }
+        }
+    } else {
+        for (i, l) in &mut langs.iter().enumerate() {
+            if i == langs.len() - 1 {
+                println!("└──{} ({})", l.name, l.projs.len());
+            } else {
+                println!("├──{} ({})", l.name, l.projs.len());
+            }
+            for (j, p) in &mut l.projs.iter().enumerate() {
+                if i == langs.len() - 1 {
+                    if j == l.projs.len() - 1 {
+                        if p.is_ok {
+                            println!("   └──{}", p.name);
+                        } else {
+                            println!("   └──{}  *", p.name);
+                        }
+                    } else if p.is_ok {
+                        println!("   ├──{}", p.name);
+                    } else {
+                        println!("   ├──{}  *", p.name);
+                    }
+                } else {
+                    if j == l.projs.len() - 1 {
+                        if p.is_ok {
+                            println!("│  └──{}", p.name);
+                        } else {
+                            println!("│  └──{}  *", p.name);
+                        }
+                    } else {
+                        if p.is_ok {
+                            println!("│  ├──{}", p.name);
+                        } else {
+                            println!("│  ├──{}  *", p.name);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn is_git_repo(path: &Path) -> bool {
+    let entries = fs::read_dir(path);
+    return match entries {
+        Ok(dir) => {
+            dir.into_iter().any(|x| {
+                match x {
+                    Ok(e) => e.file_name() == ".git",
+                    Err(_) => false,
+                }
+            })
+        }
+        Err(_) => false
+    };
+}
+
+fn list_dir(path: &str, mut depth: i32, count: &mut i32, langs: &mut Vec<Lang>) -> io::Result<()> {
+    if depth == 0 {
+        return Ok(());
+    } else {
+        depth -= 1;
+    }
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+        let pstr = path.to_str().unwrap();
+        if path.is_dir() {
+            let nstr = path.file_name().unwrap().to_str().unwrap();
+            if is_git_repo(&path) {
+                *count += 1;
+                match langs.pop() {
+                    None => {}
+                    Some(mut lang) => {
+                        lang.add_proj(Proj::new(nstr, pstr));
+                        langs.push(lang)
+                    }
+                }
+            } else {
+                if !IGNORED.contains(&nstr) {
+                    langs.push(Lang::new(nstr));
+                    list_dir(pstr, depth, count, langs)?;
+                }
+            }
+        }
+    };
+    Ok(())
+}
+
+fn check_status(proj: &mut Proj) -> bool {
+    return match process::Command::new("git")
+        .arg("-C")
+        .arg(proj.path.as_str())
+        .arg("status")
+        .arg("--porcelain")
+        .output() {
+        Ok(out) => {
+            if out.stdout.len() == 0 {
+                return true;
+            }
+            proj.is_ok = false;
+            false
+        }
+        Err(_) => {
+            proj.is_ok = false;
+            false
+        }
+    };
+}
